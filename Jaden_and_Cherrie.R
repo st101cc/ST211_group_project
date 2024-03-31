6f1d37e56cbd9dbe5cfbc58d9ba23e4dc0f3204f
st101cc <cherrietenmore@gmail.com>
2024-03-31 21:45:23 +0100

    Jaden_and_Cherrie

--- Jaden_and_Cherrie.R
+++ Jaden_and_Cherrie.R
@@ -14,8 +14,18 @@ summary(data)
 # continuous variables: "W1GrssyrHH", "W1yschat1", "W2ghq12scr", "W4schatYP", "W6DebtattYP", "W8DGHQSC", "W8QDEB2"
 
 ## plots for categorical variables
+<-ggplot(data=data, aes(x=fiveac,y=ks4score)) +
+  geom_boxplot() +
+  labs(title="Boxplot without Math & English",
+       x="5 A*-C Subjects",
+       y="Ks4score")
 
 ## plots for continuous variables
+W1GrssyrHH<-
+
+  
+  ``
+
 
 ## replace missing values
 # calculate the percentage of missing values in each column 
